1600388183a553362f9b7939338c66482f3965e4
Rio <riochr17@gmail.com>
2026-04-16 18:07:02 +0700

    update swan version

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import { AgentTool, OpenAILLM, startAgentServer, startAgentTelegram, startAgentWAHA, TerminationError, TerminationTimeout } from "@ssww.one/framework";
+import { AgentTool, loop, OpenAILLM, startAgentServer, startAgentTelegram, startAgentWAHA, TerminationError, TerminationTimeout } from "@ssww.one/framework";
 
 export async function agent(at: AgentTool) {
   const name = process.env.NAME || 'ABC Agent';
@@ -11,25 +11,14 @@ export async function agent(at: AgentTool) {
   at.print(await at.askLLM(`Give short greetings to user based on given context. Your greetings must be simple`), true);
   
   // Main loop
-  try {
-    while (true) {
-      const instruction = await at.waitForUserInstruction();
-      await at.streamLLM(
-        `User request: "${instruction}". Respond user request based on given knowledge.`,
-        (s: string) => at.print(s)
-      );
-      at.print('', true);
-    }
-  } catch (err: any) {
-    if (err instanceof TerminationError) {
-      at.exit(`Conversation ended`);
-    }
-    else if (err instanceof TerminationTimeout) {
-      at.exit(`Conversation ended`);
-    } else {
-      at.print(err?.message ?? '5XX: Agent cannot process this request', true);
-    }
-  }
+  await loop(async () => {
+    const instruction = await at.waitForUserInstruction();
+    await at.streamLLM(
+      `User request: "${instruction}". Respond user request based on given knowledge.`,
+      (s: string) => at.print(s)
+    );
+    at.print('', true);
+  });
 }
 
 startAgentServer(agent, {
